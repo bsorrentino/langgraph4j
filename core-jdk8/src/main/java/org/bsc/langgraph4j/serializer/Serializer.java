7a820294f37b9e1d361f2210295806fde5c59293
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-08-09 23:37:33 +0200

    feat: add utitlity for support serialization of nullable value

--- core-jdk8/src/main/java/org/bsc/langgraph4j/serializer/Serializer.java
+++ core-jdk8/src/main/java/org/bsc/langgraph4j/serializer/Serializer.java
@@ -4,13 +4,27 @@ import org.bsc.langgraph4j.checkpoint.Checkpoint;
 
 import java.io.*;
 import java.util.Objects;
+import java.util.Optional;
 
 public interface Serializer<T> {
 
     void write(T object, ObjectOutput out) throws IOException;
-
-
     T read(ObjectInput in) throws IOException, ClassNotFoundException ;
+
+    default void writeNullable(T object, ObjectOutput out) throws IOException {
+        if( object == null ) {
+            out.writeBoolean(false);
+        } else {
+            out.writeBoolean(true);
+            write(object, out);
+        }
+    }
+    default Optional<T> readNullable(ObjectInput in) throws IOException, ClassNotFoundException {
+        if( in.readBoolean() ) {
+            return Optional.ofNullable(read(in));
+        }
+        return Optional.empty();
+    }
 
     default byte[] writeObject(T object) throws IOException {
         Objects.requireNonNull( object, "object cannot be null" );
