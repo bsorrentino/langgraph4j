cc44dfea071d6f7333ebe68d32a41142a8b2a2b5
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-08-10 13:00:15 +0200

    build: add changelog update shell

---
+++ changelog.sh
@@ -0,0 +1,10 @@
+# prepare the CHANGELOG.md with the current version
+git flow hotfix start changeme
+
+# generate CHANGELOG.md 
+git-changelog-command-line -of CHANGELOG.md
+
+git commit -m'docs: update changeme' -a
+
+# finish the hotfix without create tag
+git flow hotfix finish changeme -n
