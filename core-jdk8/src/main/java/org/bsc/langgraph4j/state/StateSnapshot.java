cc86564cc2f27595559018635e6af48117b28275
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-08-26 22:28:51 +0200

    feat(StateSnapshot): add support StateSnaphot object
    work on #14

--- core-jdk8/src/main/java/org/bsc/langgraph4j/state/StateSnapshot.java
+++ core-jdk8/src/main/java/org/bsc/langgraph4j/state/StateSnapshot.java
@@ -1,2 +1,29 @@
-package org.bsc.langgraph4j.state;public class StateSnapshot {
+package org.bsc.langgraph4j.state;
+
+import lombok.NonNull;
+import lombok.Value;
+import lombok.var;
+import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.checkpoint.Checkpoint;
+
+@Value
+public class StateSnapshot {
+    AgentState state;
+    String next;
+    RunnableConfig config;
+
+    private StateSnapshot(@NonNull AgentState state, @NonNull String next, @NonNull RunnableConfig config) {
+        this.state = state;
+        this.next = next;
+        this.config = config;
+    }
+
+    public static StateSnapshot of(Checkpoint checkpoint, RunnableConfig config) {
+
+        var newConfig = RunnableConfig.builder(config)
+                                .checkPointId( checkpoint.getId() )
+                                .build() ;
+        return new StateSnapshot(checkpoint.getState(), checkpoint.getNextNodeId(), newConfig);
+    }
+
 }
