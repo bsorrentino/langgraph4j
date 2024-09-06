f1765769556e0242a96e1abbe6371e423cb8731b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-09-06 10:51:29 +0200

    build:  utility shells refinements

--- hotfix-changelog.sh
+++ hotfix-changelog.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # prepare the CHANGELOG.md with the current version
 git flow hotfix start changeme
 
@@ -7,4 +9,4 @@ git-changelog-command-line -of CHANGELOG.md
 git commit -m'docs: update changeme' -a
 
 # finish the hotfix without create tag
-git flow hotfix finish changeme -n
+git flow hotfix finish changeme -n -m"changelog hotfix merge"
