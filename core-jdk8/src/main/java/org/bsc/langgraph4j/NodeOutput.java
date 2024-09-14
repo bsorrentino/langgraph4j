41a095ebffff6b525151d16a57f1161a10dc3a69
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-09-14 19:06:53 +0200

    refactor(NodeOutput): remove lombok @Value and remove final class constraint
    work on #24

--- core-jdk8/src/main/java/org/bsc/langgraph4j/NodeOutput.java
+++ core-jdk8/src/main/java/org/bsc/langgraph4j/NodeOutput.java
@@ -1,25 +1,54 @@
 package org.bsc.langgraph4j;
 
-import lombok.Value;
-import lombok.experimental.Accessors;
 import org.bsc.langgraph4j.state.AgentState;
+
+import static java.lang.String.format;
 
 /**
  * Represents the output of a node in a graph.
  *
  * @param <State> the type of the state associated with the node output
  */
-@Value(staticConstructor="of")
-@Accessors(fluent = true)
 public class NodeOutput<State extends AgentState> {
+
+    public static <State extends AgentState> NodeOutput<State> of( String node, State state ) {
+        return new NodeOutput<>(node, state);
+    }
 
     /**
      * The identifier of the node.
      */
-    String node;
+    private final String node;
 
     /**
      * The state associated with the node.
      */
-    State state;
+    private final State state;
+
+    public String node() {
+        return node;
+    }
+
+    public State state() {
+        return state;
+    }
+
+    /**
+     * @deprecated Use {@link #state()} instead.
+     */
+    @Deprecated
+    public State getState( ) {
+        return state();
+    }
+
+    protected NodeOutput( String node, State state ) {
+        this.node = node;
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return format("NodeOutput{node=%s, state=%s}", node(), state());
+    }
+
 }
