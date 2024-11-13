c8916f44303b350ed8da0a62b4dc7c02130cdc44
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-11-13 18:44:04 +0100

    feat(StreamingOutput): add toString()

--- core-jdk8/src/main/java/org/bsc/langgraph4j/streaming/StreamingOutput.java
+++ core-jdk8/src/main/java/org/bsc/langgraph4j/streaming/StreamingOutput.java
@@ -3,6 +3,8 @@ package org.bsc.langgraph4j.streaming;
 import org.bsc.langgraph4j.NodeOutput;
 import org.bsc.langgraph4j.state.AgentState;
 
+import static java.lang.String.format;
+
 public class StreamingOutput<State extends AgentState> extends NodeOutput<State> {
 
     private final String chunk; // null
@@ -16,4 +18,13 @@ public class StreamingOutput<State extends AgentState> extends NodeOutput<State>
     public String chunk() {
         return chunk;
     }
+
+    @Override
+    public String toString() {
+        if( node() == null ) {
+            return format("StreamingOutput{chunk=%s}", chunk());
+        }
+        return format("StreamingOutput{node=%s, state=%s, chunk=%s}", node(), state(), chunk());
+    }
+
 }
