aaf57f15199df40aa0ac67320a5b7f703dd182b7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-11-25 23:13:11 +0100

    feat: adds node action that accept either Map and RunnableConfig
    work on #39

---
+++ core-jdk8/src/main/java/org/bsc/langgraph4j/action/NodeActionWithConfig.java
@@ -0,0 +1,13 @@
+package org.bsc.langgraph4j.action;
+
+
+import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.state.AgentState;
+
+import java.util.Map;
+
+@FunctionalInterface
+public interface NodeActionWithConfig<S extends AgentState> {
+    Map<String, Object> apply(S t, RunnableConfig config) throws Exception;
+
+}
