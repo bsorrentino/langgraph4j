b731612bd46284ab8a9f0aef7b7d2e0604665ee7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-12-15 13:00:45 +0100

    docs: added comprehensive documentation

--- agent-executor/src/main/java/org/bsc/langgraph4j/agentexecutor/serializer/jackson/JSONStateSerializer.java
+++ agent-executor/src/main/java/org/bsc/langgraph4j/agentexecutor/serializer/jackson/JSONStateSerializer.java
@@ -18,8 +18,15 @@ import org.bsc.langgraph4j.serializer.plain_text.jackson.JacksonStateSerializer;
 import java.io.*;
 import java.util.*;
 
+/**
+ * JSONStateSerializer is a class that extends JacksonStateSerializer for serializing and deserializing
+ * the state of an AgentExecutor using JSON format.
+ */
 public class JSONStateSerializer extends JacksonStateSerializer<AgentExecutor.State> {
 
+    /**
+     * Constructs a new JSONStateSerializer and registers custom deserializers for various agent-related classes.
+     */
     public JSONStateSerializer() {
         super( AgentExecutor.State::new );
 
@@ -33,12 +40,27 @@ public class JSONStateSerializer extends JacksonStateSerializer<AgentExecutor.St
         );
     }
 
+    /**
+     * Serializes the given AgentExecutor.State object to an output stream in JSON format.
+     *
+     * @param object the AgentExecutor.State object to serialize
+     * @param out the output stream to write the serialized object to
+     * @throws IOException if an I/O error occurs during serialization
+     */
     @Override
     public void write(AgentExecutor.State object, ObjectOutput out) throws IOException {
         var json = objectMapper.writeValueAsString(object);
         out.writeUTF(json);
     }
 
+    /**
+     * Deserializes an AgentExecutor.State object from an input stream in JSON format.
+     *
+     * @param in the input stream to read the serialized object from
+     * @return the deserialized AgentExecutor.State object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     @Override
     public AgentExecutor.State read(ObjectInput in) throws IOException, ClassNotFoundException {
         var json = in.readUTF();
@@ -47,8 +69,22 @@ public class JSONStateSerializer extends JacksonStateSerializer<AgentExecutor.St
 
 }
 
-class IntermediateStepDeserializer extends  JsonDeserializer<IntermediateStep> {
 
+/**
+ * Deserializer for the IntermediateStep class, extending JsonDeserializer.
+ * This class is responsible for converting JSON data into an IntermediateStep object.
+ */
+class IntermediateStepDeserializer extends JsonDeserializer<IntermediateStep> {
+
+    /**
+     * Deserializes JSON data into an IntermediateStep object.
+     *
+     * @param parser the JsonParser used to read the JSON data
+     * @param ctx the DeserializationContext for reading values
+     * @return an IntermediateStep object created from the JSON data
+     * @throws IOException if an input or output exception occurs
+     * @throws JacksonException if a Jackson-specific exception occurs
+     */
     @Override
     public IntermediateStep deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JacksonException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -61,8 +97,22 @@ class IntermediateStepDeserializer extends  JsonDeserializer<IntermediateStep> {
     }
 }
 
-class ToolExecutionRequestDeserializer extends  JsonDeserializer<ToolExecutionRequest> {
 
+/**
+ * Deserializer for the ToolExecutionRequest class.
+ * This class extends JsonDeserializer to provide custom deserialization logic for ToolExecutionRequest objects.
+ */
+class ToolExecutionRequestDeserializer extends JsonDeserializer<ToolExecutionRequest> {
+
+    /**
+     * Deserializes a JSON representation of a ToolExecutionRequest.
+     *
+     * @param parser the JsonParser used to read the JSON data
+     * @param ctx the DeserializationContext that can be used to access additional information during deserialization
+     * @return a ToolExecutionRequest object populated with data from the JSON
+     * @throws IOException if there is an issue reading the JSON data
+     * @throws JacksonException if there is a problem with Jackson processing
+     */
     @Override
     public ToolExecutionRequest deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JacksonException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -74,8 +124,21 @@ class ToolExecutionRequestDeserializer extends  JsonDeserializer<ToolExecutionRe
     }
 }
 
-class AgentActionDeserializer extends  JsonDeserializer<AgentAction> {
 
+/**
+ * Deserializes JSON data into an AgentAction object.
+ */
+class AgentActionDeserializer extends JsonDeserializer<AgentAction> {
+
+    /**
+     * Deserializes the JSON representation of an AgentAction.
+     *
+     * @param parser the JsonParser used to read the JSON data
+     * @param ctx the DeserializationContext for reading values
+     * @return the deserialized AgentAction object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws JacksonException if a Jackson-specific error occurs
+     */
     @Override
     public AgentAction deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JacksonException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -90,8 +153,22 @@ class AgentActionDeserializer extends  JsonDeserializer<AgentAction> {
     }
 }
 
-class AgentFinishDeserializer extends  JsonDeserializer<AgentFinish> {
 
+/**
+ * Deserializer for the AgentFinish class, extending JsonDeserializer.
+ * This class is responsible for converting JSON data into an AgentFinish object.
+ */
+class AgentFinishDeserializer extends JsonDeserializer<AgentFinish> {
+
+    /**
+     * Deserializes JSON data into an AgentFinish object.
+     *
+     * @param parser the JsonParser used to read the JSON data
+     * @param ctx the DeserializationContext for the deserialization process
+     * @return an instance of AgentFinish populated with data from the JSON
+     * @throws IOException if an input or output exception occurs
+     * @throws JacksonException if a Jackson-specific exception occurs
+     */
     @Override
     public AgentFinish deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JacksonException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -115,8 +192,22 @@ class AgentFinishDeserializer extends  JsonDeserializer<AgentFinish> {
     }
 }
 
-class AgentOutcomeDeserializer extends  JsonDeserializer<AgentOutcome> {
 
+/**
+ * Deserializes JSON data into an instance of {@link AgentOutcome}.
+ * This class extends {@link JsonDeserializer} to provide custom deserialization logic.
+ */
+class AgentOutcomeDeserializer extends JsonDeserializer<AgentOutcome> {
+
+    /**
+     * Deserializes the JSON content into an {@link AgentOutcome} object.
+     *
+     * @param parser the JSON parser used to read the JSON content
+     * @param ctx the deserialization context
+     * @return an instance of {@link AgentOutcome} populated with the deserialized data
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws JacksonException if a Jackson-specific error occurs during deserialization
+     */
     @Override
     public AgentOutcome deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JacksonException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -135,8 +226,24 @@ class AgentOutcomeDeserializer extends  JsonDeserializer<AgentOutcome> {
     }
 }
 
+
+/**
+ * The StateDeserializer class is responsible for deserializing JSON data 
+ * into an instance of AgentExecutor.State. It extends the JsonDeserializer 
+ * class provided by the Jackson library.
+ */
 class StateDeserializer extends JsonDeserializer<AgentExecutor.State> {
 
+    /**
+     * Deserializes JSON data from the given parser into an 
+     * AgentExecutor.State object.
+     *
+     * @param parser the JsonParser used to read the JSON data
+     * @param ctx the DeserializationContext used for deserialization
+     * @return an instance of AgentExecutor.State populated with the deserialized data
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws JsonProcessingException if there is a problem processing the JSON
+     */
     @Override
     public AgentExecutor.State deserialize(JsonParser parser, DeserializationContext ctx) throws IOException, JsonProcessingException {
         JsonNode node = parser.getCodec().readTree(parser);
@@ -170,4 +277,3 @@ class StateDeserializer extends JsonDeserializer<AgentExecutor.State> {
         return new AgentExecutor.State( data );
     }
 }
-
--- agent-executor/src/main/java/org/bsc/langgraph4j/agentexecutor/serializer/std/STDStateSerializer.java
+++ agent-executor/src/main/java/org/bsc/langgraph4j/agentexecutor/serializer/std/STDStateSerializer.java
@@ -18,8 +18,16 @@ import java.util.Map;
 
 
 
+/**
+ * The STDStateSerializer class is responsible for serializing the state of the AgentExecutor.
+ * It extends the ObjectStreamStateSerializer with a specific type of AgentExecutor.State.
+ */
 public class STDStateSerializer extends ObjectStreamStateSerializer<AgentExecutor.State> {
 
+    /**
+     * Constructs a new instance of STDStateSerializer.
+     * It initializes the serializer by registering various classes with their corresponding serializers.
+     */
     public STDStateSerializer() {
         super(AgentExecutor.State::new);
 
@@ -31,8 +39,20 @@ public class STDStateSerializer extends ObjectStreamStateSerializer<AgentExecuto
     }
 }
 
+
+/**
+ * The AgentActionSerializer class implements the Serializer interface for the AgentAction type.
+ * It provides methods to serialize and deserialize AgentAction objects.
+ */
 class AgentActionSerializer implements Serializer<AgentAction> {
 
+    /**
+     * Serializes the given AgentAction object to the specified output stream.
+     *
+     * @param action the AgentAction object to serialize
+     * @param out the output stream to write the serialized data to
+     * @throws IOException if an I/O error occurs during serialization
+     */
     @Override
     public void write(AgentAction action, ObjectOutput out) throws IOException {
         ToolExecutionRequest ter =  action.toolExecutionRequest();
@@ -40,9 +60,16 @@ class AgentActionSerializer implements Serializer<AgentAction> {
         out.writeUTF( ter.name() );
         out.writeUTF( ter.arguments() );
         out.writeUTF( action.log() );
-
     }
 
+    /**
+     * Deserializes an AgentAction object from the specified input stream.
+     *
+     * @param in the input stream to read the serialized data from
+     * @return the deserialized AgentAction object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     @Override
     public AgentAction read(ObjectInput in) throws IOException, ClassNotFoundException {
         ToolExecutionRequest ter = ToolExecutionRequest.builder()
@@ -52,18 +79,37 @@ class AgentActionSerializer implements Serializer<AgentAction> {
                 .build();
 
         return  new AgentAction(  ter, in.readUTF() );
-
     }
 }
 
+
+/**
+ * The AgentFinishSerializer class implements the Serializer interface for the AgentFinish type.
+ * It provides methods to serialize and deserialize AgentFinish objects.
+ */
 class AgentFinishSerializer implements Serializer<AgentFinish> {
 
+    /**
+     * Serializes the given AgentFinish object to the specified ObjectOutput.
+     *
+     * @param object the AgentFinish object to serialize
+     * @param out the ObjectOutput to write the serialized data to
+     * @throws IOException if an I/O error occurs during serialization
+     */
     @Override
     public void write(AgentFinish object, ObjectOutput out) throws IOException {
         out.writeObject(object.returnValues());
         out.writeUTF(object.log());
     }
 
+    /**
+     * Deserializes an AgentFinish object from the specified ObjectInput.
+     *
+     * @param in the ObjectInput to read the serialized data from
+     * @return the deserialized AgentFinish object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     @Override
     @SuppressWarnings("unchecked")
     public AgentFinish read(ObjectInput in) throws IOException, ClassNotFoundException {
@@ -74,28 +120,71 @@ class AgentFinishSerializer implements Serializer<AgentFinish> {
 
 }
 
+
+/**
+ * The AgentOutcomeSerializer class is responsible for serializing and deserializing 
+ * AgentOutcome objects. It implements the NullableObjectSerializer interface, 
+ * providing methods to write and read nullable objects.
+ */
 class AgentOutcomeSerializer implements NullableObjectSerializer<AgentOutcome> {
+    
+    /**
+     * Serializes the given AgentOutcome object to the specified output stream.
+     *
+     * @param object the AgentOutcome object to serialize
+     * @param out the output stream to write the serialized object to
+     * @throws IOException if an I/O error occurs during serialization
+     */
     @Override
     public void write(AgentOutcome object, ObjectOutput out) throws IOException {
         writeNullableObject(object.action(), out);
         writeNullableObject(object.finish(), out);
     }
 
+    /**
+     * Deserializes an AgentOutcome object from the specified input stream.
+     *
+     * @param in the input stream to read the serialized object from
+     * @return the deserialized AgentOutcome object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     @Override
     public AgentOutcome read(ObjectInput in) throws IOException, ClassNotFoundException {
-        AgentAction action = readNullableObject( in ).map(AgentAction.class::cast).orElse(null);
-        AgentFinish finish = readNullableObject( in ).map(AgentFinish.class::cast).orElse(null);
+        AgentAction action = readNullableObject(in).map(AgentAction.class::cast).orElse(null);
+        AgentFinish finish = readNullableObject(in).map(AgentFinish.class::cast).orElse(null);
         return new AgentOutcome(action, finish);
     }
 }
 
+
+/**
+ * The IntermediateStepSerializer class implements the Serializer interface for the IntermediateStep type.
+ * It provides methods to serialize and deserialize IntermediateStep objects.
+ */
 class IntermediateStepSerializer implements Serializer<IntermediateStep> {
+    
+    /**
+     * Serializes the given IntermediateStep object to the specified ObjectOutput.
+     *
+     * @param object the IntermediateStep object to serialize
+     * @param out the ObjectOutput to write the serialized data to
+     * @throws IOException if an I/O error occurs during serialization
+     */
     @Override
     public void write(IntermediateStep object, ObjectOutput out) throws IOException {
         out.writeUTF(object.observation());
         out.writeObject(object.action());
     }
 
+    /**
+     * Deserializes an IntermediateStep object from the specified ObjectInput.
+     *
+     * @param in the ObjectInput to read the serialized data from
+     * @return the deserialized IntermediateStep object
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     @Override
     public IntermediateStep read(ObjectInput in) throws IOException, ClassNotFoundException {
         String observation = in.readUTF();
