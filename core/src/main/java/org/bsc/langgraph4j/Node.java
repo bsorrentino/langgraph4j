d50f56c5937251f77b931b8d0f77fb01db69a310
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-01-28 20:57:31 +0100

    feat(Node): add parallel execution support
    work on #72

--- core/src/main/java/org/bsc/langgraph4j/Node.java
+++ core/src/main/java/org/bsc/langgraph4j/Node.java
@@ -2,10 +2,13 @@ package org.bsc.langgraph4j;
 
 import org.bsc.langgraph4j.action.AsyncNodeActionWithConfig;
 import org.bsc.langgraph4j.state.AgentState;
+import org.bsc.langgraph4j.state.Channel;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+
+import static java.lang.String.format;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 /**
  * Represents a node in a graph, characterized by a unique identifier and a factory for creating
@@ -17,7 +20,9 @@ import java.util.function.Function;
  * @param actionFactory a factory function that takes a {@link CompileConfig} and returns an
  *                      {@link AsyncNodeActionWithConfig} instance for the specified {@code State}.
  */
-record Node<State extends AgentState>(String id, ActionFactory<State> actionFactory) {
+record Node<State extends AgentState>(String id, ActionFactory<State> actionFactory)  {
+
+    public static final String PARALLEL_PREFIX = "__PARALLEL__";
 
     interface ActionFactory<State extends AgentState> {
         AsyncNodeActionWithConfig<State> apply( CompileConfig config ) throws GraphStateException;
@@ -32,8 +37,13 @@ record Node<State extends AgentState>(String id, ActionFactory<State> actionFact
         this(id, null);
     }
 
+    public boolean isParallel() {
+        return id.startsWith(PARALLEL_PREFIX);
+    }
+
     /**
      * Checks if this node is equal to another object.
+     *
      * @param o the object to compare with
      * @return true if this node is equal to the specified object, false otherwise
      */
@@ -54,4 +64,33 @@ record Node<State extends AgentState>(String id, ActionFactory<State> actionFact
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    static <State extends AgentState> Node<State> parallel(String id, List<AsyncNodeActionWithConfig<State>> actions, Map<String, Channel<?>> channels) {
+        return new Node<>( format( "%s(%s)", PARALLEL_PREFIX, id), (config ) -> new AsyncParallelNodeAction<>( actions, channels ));
+    }
+
 }
+
+record AsyncParallelNodeAction<State extends AgentState>(
+        List<AsyncNodeActionWithConfig<State>> actions,
+        Map<String, Channel<?>> channels ) implements AsyncNodeActionWithConfig<State> {
+
+    @Override
+    public CompletableFuture<Map<String, Object>> apply(State state, RunnableConfig config) {
+        final var partialMergedStates = new HashMap<String, Object>();
+        var futures = actions.stream()
+                .map(action ->
+                        action.apply(state, config).thenApply(partialState -> {
+                            var updatedState = AgentState.updateState( partialMergedStates, partialState, channels);
+                            partialMergedStates.putAll(updatedState);
+                            return action;
+                        }) )
+                //.map( future -> supplyAsync(future::join) )
+                .toList()
+                .toArray(new CompletableFuture[0]);
+        return CompletableFuture.allOf(futures)
+                .thenApply((p) -> partialMergedStates);
+    }
+
+}
+
