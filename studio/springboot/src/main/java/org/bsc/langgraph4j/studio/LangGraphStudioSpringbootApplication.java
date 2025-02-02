1df22b7aa8dc43e459a899b2905eeac809204f35
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-02-02 15:51:43 +0100

    feat(studio): setup new module for run studio from springboot

---
+++ studio/springboot/src/main/java/org/bsc/langgraph4j/studio/LangGraphStudioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.bsc.langgraph4j.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangGraphStudioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangGraphStudioSpringbootApplication.class, args);
+	}
+
+}
---
+++ studio/springboot/src/test/java/org/bsc/langgraph4j/studio/Langgraph4jStudioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsc.langgraph4j.studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langgraph4jStudioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
