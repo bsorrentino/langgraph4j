f98c40ee56e30e1f438a8291d03342b8ba088b5f
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-02-07 00:49:03 +0100

    refactor(core/Node.java): introduce interfaces and classes for subgraph handling
    Refactored SubGraphNode to interface, added concrete implementations for different types of subgraph nodes (State, Compiled).

--- core/src/main/java/org/bsc/langgraph4j/Node.java
+++ core/src/main/java/org/bsc/langgraph4j/Node.java
@@ -133,11 +133,17 @@ class ParallelNode<State extends AgentState> extends Node<State> {
     }
 }
 
-class SubGraphNode<State extends AgentState> extends Node<State> {
+interface SubGraphNode<State extends AgentState> {
+
+    StateGraph<State> subGraph();
+
+}
+
+class SubStateGraphNode<State extends AgentState> extends Node<State> implements SubGraphNode<State>  {
 
     private final StateGraph<State> subGraph;
 
-    public SubGraphNode(@NonNull String id, @NonNull  StateGraph<State> subGraph ) {
+    public SubStateGraphNode(@NonNull String id, @NonNull  StateGraph<State> subGraph ) {
         super(id);
         this.subGraph = subGraph;
     }
@@ -148,5 +154,63 @@ class SubGraphNode<State extends AgentState> extends Node<State> {
 
     public String formatId( String nodeId ) {
         return format("%s@%s", id(), nodeId );
+    }
+}
+
+class SubCompiledGraphNode<State extends AgentState> extends Node<State> implements SubGraphNode<State> {
+
+    private final CompiledGraph<State> subGraph;
+
+    public SubCompiledGraphNode(@NonNull String id, @NonNull  CompiledGraph<State> subGraph ) {
+        super(id, (config ) -> new SubgraphNodeAction<>(subGraph) );
+        this.subGraph = subGraph;
+    }
+
+    public StateGraph<State> subGraph() {
+        return subGraph.stateGraph;
+    }
+}
+
+/**
+ * Represents an action to perform a subgraph on a given state with a specific configuration.
+ *
+ * <p>This record encapsulates the behavior required to execute a compiled graph using a provided state.
+ * It implements the {@link AsyncNodeActionWithConfig} interface, ensuring that the execution is handled asynchronously with the ability to configure settings.</p>
+ *
+ * @param <State> The type of state the subgraph operates on, which must extend {@link AgentState}.
+ * @param subGraph sub graph instance
+ * @see CompiledGraph
+ * @see AsyncNodeActionWithConfig
+ */
+record SubgraphNodeAction<State extends AgentState>(
+        CompiledGraph<State> subGraph) implements AsyncNodeActionWithConfig<State> {
+
+    /**
+     * Executes the given graph with the provided state and configuration.
+     *
+     * @param state  The current state of the system, containing input data and intermediate results.
+     * @param config The configuration for the graph execution.
+     * @return A {@link CompletableFuture} that will complete with a result of type {@code Map<String, Object>}.
+     * If an exception occurs during execution, the future will be completed exceptionally.
+     */
+    @Override
+    public CompletableFuture<Map<String, Object>> apply(State state, RunnableConfig config) {
+        CompletableFuture<Map<String, Object>> future = new CompletableFuture<>();
+
+        try {
+            final Map<String, Object> input = (subGraph.compileConfig.checkpointSaver().isPresent()) ?
+                    Map.of() :
+                    state.data();
+
+            var generator = subGraph.stream(input, config);
+
+            future.complete(Map.of("_subgraph", generator));
+
+        } catch (Exception e) {
+
+            future.completeExceptionally(e);
+        }
+
+        return future;
     }
 }
