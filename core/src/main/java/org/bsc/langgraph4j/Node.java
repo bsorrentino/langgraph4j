79f83954ba0308c2abe987c9c9f84714bf714525
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-02-07 00:02:13 +0100

    fix(core/Node.java): Ensured the `equals` method only casts to `Node<?>` if the object is not null,
    work on #73

--- core/src/main/java/org/bsc/langgraph4j/Node.java
+++ core/src/main/java/org/bsc/langgraph4j/Node.java
@@ -78,9 +78,12 @@ class Node<State extends AgentState> {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node<?> node = (Node<?>) o;
-        return Objects.equals(id, node.id);
+        if (o == null ) return false;
+        if( o instanceof Node<?> node ) {
+            return Objects.equals(id, node.id);
+        }
+        return false;
+
     }
 
     /**
