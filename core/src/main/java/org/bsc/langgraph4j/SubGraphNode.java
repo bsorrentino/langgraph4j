5ef408ca9d5987a3cb536d597d6e76dcf69f0cc9
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-02-07 20:07:34 +0100

    feat(SubGraphNode.java): introduce SubGraphNode interface
    Add a new interface `SubGraphNode` in package `org.bsc.langgraph4j`, which defines methods related to managing sub-graph nodes with specific states.
    This interface includes a method for obtaining the sub-graph and a static utility method for formatting node IDs to distinct subgraph nodes from standard ones
    
    work on #73

---
+++ core/src/main/java/org/bsc/langgraph4j/SubGraphNode.java
@@ -0,0 +1,17 @@
+package org.bsc.langgraph4j;
+
+import org.bsc.langgraph4j.state.AgentState;
+
+import static java.lang.String.format;
+
+interface SubGraphNode<State extends AgentState> {
+    String PREFIX_FORMAT = "(%s)%s";
+
+    StateGraph<State> subGraph();
+
+    static String formatId(String subGraphNodeId, String nodeId ) {
+        return format(PREFIX_FORMAT, subGraphNodeId, nodeId );
+    }
+
+}
+
