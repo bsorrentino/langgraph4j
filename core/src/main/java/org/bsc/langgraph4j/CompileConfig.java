4729fd8cceabdcb44b48f1454816b0365afbf96b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-02-10 19:29:11 +0100

    refactor(core/CompileConfig.java): update interrupt fields to List
    - Changed `interruptBefore` and `interruptAfter` from String arrays to Lists to allow for more flexible configuration.
    - Deprecated the old getter methods in favor of new methods that return immutable lists.
    - Add copy constructor.

--- core/src/main/java/org/bsc/langgraph4j/CompileConfig.java
+++ core/src/main/java/org/bsc/langgraph4j/CompileConfig.java
@@ -1,8 +1,9 @@
 package org.bsc.langgraph4j;
 
-import lombok.Getter;
 import org.bsc.langgraph4j.checkpoint.BaseCheckpointSaver;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
@@ -11,30 +12,49 @@ import static java.util.Optional.ofNullable;
 public class CompileConfig {
 
     private BaseCheckpointSaver checkpointSaver;
-    @Getter
-    private String[] interruptBefore = {};
-    @Getter
-    private String[] interruptAfter = {};
+    private List<String> interruptsBefore = List.of();
+    private List<String> interruptsAfter = List.of();
 
+    @Deprecated
+    public String[] getInterruptBefore() { return interruptsBefore.toArray( new String[0]); }
+    @Deprecated
+    public String[] getInterruptAfter() { return interruptsAfter.toArray( new String[0]); }
+
+    public List<String> interruptsBefore() { return interruptsBefore; }
+    public List<String> interruptsAfter() { return interruptsAfter; }
     public Optional<BaseCheckpointSaver> checkpointSaver() { return ofNullable(checkpointSaver); }
 
     public static Builder builder() {
-        return new Builder();
+        return new Builder(new CompileConfig());
+    }
+    public static Builder builder( CompileConfig config ) {
+        return new Builder(config);
     }
 
     public static class Builder {
-        private final CompileConfig config = new CompileConfig();
+        private final CompileConfig config;
 
+        protected Builder( CompileConfig config ) {
+            this.config = new CompileConfig(config);
+        }
         public Builder checkpointSaver(BaseCheckpointSaver checkpointSaver) {
             this.config.checkpointSaver = checkpointSaver;
             return this;
         }
         public Builder interruptBefore(String... interruptBefore) {
-            this.config.interruptBefore = interruptBefore;
+            this.config.interruptsBefore = List.of(interruptBefore);
             return this;
         }
         public Builder interruptAfter(String... interruptAfter) {
-            this.config.interruptAfter = interruptAfter;
+            this.config.interruptsAfter = List.of(interruptAfter);
+            return this;
+        }
+        public Builder interruptsBefore(Collection<String> interruptsBefore) {
+            this.config.interruptsBefore = interruptsBefore.stream().toList();
+            return this;
+        }
+        public Builder interruptsAfter(Collection<String> interruptsAfter) {
+            this.config.interruptsAfter = interruptsAfter.stream().toList();
             return this;
         }
         public CompileConfig build() {
@@ -44,5 +64,11 @@ public class CompileConfig {
 
 
     private CompileConfig() {}
+
+    private CompileConfig( CompileConfig config ) {
+        this.checkpointSaver = config.checkpointSaver;
+        this.interruptsBefore = config.interruptsBefore;
+        this.interruptsAfter = config.interruptsAfter;
+    }
 
 }
