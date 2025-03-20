34d02f3a7d5ac603e1a5b2354e46916053df7150
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-03-20 15:36:34 +0100

    build(studio): update webui distribution
    work on #74

--- studio/jetty/src/test/java/org/bsc/langgraph4j/jetty/LangGraphStreamingServerTest.java
+++ studio/jetty/src/test/java/org/bsc/langgraph4j/jetty/LangGraphStreamingServerTest.java
@@ -2,11 +2,13 @@ package org.bsc.langgraph4j.jetty;
 
 import org.bsc.langgraph4j.CompileConfig;
 import org.bsc.langgraph4j.StateGraph;
+import org.bsc.langgraph4j.action.AsyncNodeAction;
 import org.bsc.langgraph4j.action.EdgeAction;
 import org.bsc.langgraph4j.state.AgentState;
 import org.bsc.langgraph4j.studio.jetty.LangGraphStreamingServerJetty;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import static org.bsc.langgraph4j.StateGraph.END;
 import static org.bsc.langgraph4j.StateGraph.START;
@@ -15,8 +17,8 @@ import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
 
 public class LangGraphStreamingServerTest {
 
-
     public static void main(String[] args) throws Exception {
+
         EdgeAction<AgentState> conditionalAge  = new EdgeAction<>() {
             int steps= 0;
             @Override
@@ -29,15 +31,33 @@ public class LangGraphStreamingServerTest {
             }
         };
 
-        StateGraph<AgentState> workflow = new StateGraph<>(AgentState::new)
-            .addNode("agent", node_async((state ) -> {
-                System.out.println("agent ");
-                System.out.println(state);
+        AsyncNodeAction<AgentState> agentNode = (state ) -> {
+
+            var result = new CompletableFuture<Map<String,Object>>();
+
+            System.out.println("agent ");
+            System.out.println(state);
+
+            try {
+                Thread.sleep( 2000 );
+
                 if( state.value( "action_response").isPresent() ) {
-                    return Map.of("agent_summary", "This is just a DEMO summary");
+                    result.complete( Map.of("agent_summary", "This is just a DEMO summary") );
                 }
-                return Map.of("agent_response", "This is an Agent DEMO response");
-            }) )
+                else {
+                    result.complete( Map.of("agent_response", "This is an Agent DEMO response") );
+                }
+
+            } catch (InterruptedException e) {
+                result.completeExceptionally(e);
+            }
+
+            return result;
+
+        };
+
+        StateGraph<AgentState> workflow = new StateGraph<>(AgentState::new)
+            .addNode("agent", agentNode )
             .addNode("action", node_async( state  -> {
                 System.out.print( "action: ");
                 System.out.println( state );
@@ -50,14 +70,14 @@ public class LangGraphStreamingServerTest {
             ;
 
         var server = LangGraphStreamingServerJetty.builder()
-                                                .port(8080)
-                                                .title("LANGGRAPH4j STUDIO - DEMO")
-                                                .addInputStringArg("input")
-                                                .stateGraph(workflow)
-                                                .compileConfig(CompileConfig.builder()
-                                                        .interruptBefore( "action" )
-                                                        .build())
-                                                .build();
+                            .port(8080)
+                            .title("LANGGRAPH4j STUDIO - DEMO")
+                            .addInputStringArg("input")
+                            .stateGraph(workflow)
+                            .compileConfig(CompileConfig.builder()
+                                    //.interruptBefore( "action" )
+                                    .build())
+                            .build();
 
         server.start().join();
 
