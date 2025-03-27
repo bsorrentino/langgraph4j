65d6c1a0889eb0fd7c74d1ca99cbc34dae7bb4e8
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-03-27 16:18:09 +0100

    test: include test for check intrruptions ssenarios also on subgraph
    work on #100

--- how-tos/src/main/test/org/bsc/langgraph4j/Issue99Test.java
+++ how-tos/src/main/test/org/bsc/langgraph4j/Issue99Test.java
@@ -9,8 +9,7 @@ import static org.bsc.langgraph4j.StateGraph.END;
 import static org.bsc.langgraph4j.StateGraph.START;
 import static org.bsc.langgraph4j.action.AsyncEdgeAction.edge_async;
 import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 import java.util.Optional;
@@ -175,6 +174,73 @@ public class Issue99Test {
         assertEquals( intentRecognizeNode.getIntent() ,result.get().intent().orElseThrow() );
 
 
+
+    }
+
+
+    private <S extends AgentState> NodeOutput<S> evaluateWithConfig( StateGraph<S> graph, CompileConfig config ) throws GraphStateException {
+        var workflow = graph.compile(config);
+
+        var result = workflow.stream( Map.of() )
+                .stream()
+                .peek(System.out::println)
+                .reduce((a, b) -> b)
+                ;
+
+        assertTrue( result.isPresent() );
+        return result.get();
+    }
+
+    @Test
+    public void stateSubgraphWithInterruptionTest() throws GraphStateException {
+
+        var subGraph1 = subGraph1();
+        var subGraph2 = subGraph2();
+
+        var intentRecognizeNode = new IntentRecognizeNode();
+
+        // MainGraph Definition
+        var graph = new StateGraph<>( State::new )
+                .addNode("intent_recognize", node_async(intentRecognizeNode))
+                .addNode("subAgent1", subGraph1)
+                .addNode("subAgent2", subGraph2)
+                .addEdge(START, "intent_recognize")
+                .addConditionalEdges("intent_recognize",
+                        edge_async( state ->
+                                state.intent().orElseThrow() ),
+                        Map.of("explain", "subAgent1",
+                                "query", "subAgent2"
+                        )
+                )
+                .addEdge("subAgent1", END)
+                .addEdge("subAgent2", END)
+                ;
+
+        // EXPLAIN
+        intentRecognizeNode.setIntent("explain");
+        var result = evaluateWithConfig( graph,
+                CompileConfig.builder()
+                        .interruptBefore("subAgent1")
+                        .build() );
+
+        assertNull( result.state().data().get("step") );
+        assertEquals( intentRecognizeNode.getIntent() ,result.state().intent().orElseThrow() );
+
+        result = evaluateWithConfig( graph,
+                CompileConfig.builder()
+                        .interruptAfter("work")
+                        .build() );
+        assertEquals( "work1",result.state().data().get("step") );
+        assertEquals( intentRecognizeNode.getIntent() ,result.state().intent().orElseThrow() );
+
+        // QUERY
+        intentRecognizeNode.setIntent("query");
+        result = evaluateWithConfig( graph,
+                CompileConfig.builder()
+                        .interruptAfter("work")
+                        .build() );
+        assertEquals( "work2",result.state().data().get("step") );
+        assertEquals( intentRecognizeNode.getIntent() ,result.state().intent().orElseThrow() );
 
     }
 
