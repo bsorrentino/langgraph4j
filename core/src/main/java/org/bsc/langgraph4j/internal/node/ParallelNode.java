a05fa54792963730986072ef7989e7b3bdf1ed55
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-04-01 23:54:51 +0200

    feat: add support for compiled graph in parallel branch
    - Refactored `getEmbedGenerator` to correcly merge result states
    - Removed generator entry before updating currentState.
    - Simplified state updating logic in `ParallelNode`.
    - Added unique identifier for subgraph in state
    - Updated tests for parallel branches in MessagesStateGraph workflow with multiple compiled subgraphs.
    
    resolve #104

--- core/src/main/java/org/bsc/langgraph4j/internal/node/ParallelNode.java
+++ core/src/main/java/org/bsc/langgraph4j/internal/node/ParallelNode.java
@@ -1,16 +1,18 @@
 package org.bsc.langgraph4j.internal.node;
 
+import org.bsc.async.AsyncGenerator;
+import org.bsc.langgraph4j.NodeOutput;
 import org.bsc.langgraph4j.RunnableConfig;
 import org.bsc.langgraph4j.action.AsyncNodeActionWithConfig;
 import org.bsc.langgraph4j.state.AgentState;
 import org.bsc.langgraph4j.state.Channel;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 
 public class ParallelNode<State extends AgentState> extends Node<State> {
     public static final String PARALLEL_PREFIX = "__PARALLEL__";
@@ -19,21 +21,50 @@ public class ParallelNode<State extends AgentState> extends Node<State> {
             List<AsyncNodeActionWithConfig<State>> actions,
             Map<String, Channel<?>> channels ) implements AsyncNodeActionWithConfig<State> {
 
+        private CompletableFuture<Map<String,Object>> evalGenerator(AsyncGenerator<NodeOutput<State>> generator, Map<String,Object> initPartialState )
+        {
+            return generator.collectAsync( new ArrayList<>(), ArrayList::add)
+                    .thenApply( list -> {
+                        Map<String,Object> result = initPartialState;
+                        for( var output : list ) {
+                            result = AgentState.updateState( result, output.state().data(), channels);
+                        }
+                        return result;
+            });
+        }
+
+        @SuppressWarnings("unchecked")
+        private CompletableFuture<Map<String,Object>> evalNodeAction( AsyncNodeActionWithConfig<State> action, State state, RunnableConfig config ) {
+            return action.apply(state, config).thenCompose( partialState ->
+                partialState.entrySet().stream()
+                        .filter( e -> e.getValue() instanceof AsyncGenerator )
+                        .findFirst()
+                        .map( generatorEntry -> {
+
+                            var partialStateWithoutGenerator = partialState.entrySet().stream()
+                                    .filter( e -> !Objects.equals(e.getKey(),generatorEntry.getKey()))
+                                    .collect( Collectors.toMap( Map.Entry::getKey, Map.Entry::getValue));
+                            return evalGenerator( (AsyncGenerator<NodeOutput<State>>)generatorEntry.getValue(), partialStateWithoutGenerator );
+
+                        })
+                        .orElse( completedFuture(partialState) )
+            );
+        }
+
         @Override
         public CompletableFuture<Map<String, Object>> apply(State state, RunnableConfig config) {
-            final var partialMergedStates = new HashMap<String, Object>();
-            var futures = actions.stream()
-                    .map(action ->
-                            action.apply(state, config).thenApply(partialState -> {
-                                var updatedState = AgentState.updateState( partialMergedStates, partialState, channels);
-                                partialMergedStates.putAll(updatedState);
-                                return action;
-                            }) )
-                    //.map( future -> supplyAsync(future::join) )
-                    .toList()
-                    .toArray(new CompletableFuture[0]);
-            return CompletableFuture.allOf(futures)
-                    .thenApply((p) -> partialMergedStates);
+
+            return actions.stream()
+                    .map(action -> evalNodeAction(action, state, config) )
+                    .reduce(
+                        completedFuture(new HashMap<String,Object>()),
+                        ( futureResult, futureActionResult ) ->
+                            futureResult.thenCombine( futureActionResult, (result, actionResult) ->
+                                AgentState.updateState( result, actionResult, channels) ) /* ,
+                        (f1, f2) ->
+                                f1.thenCombine( f2, ( r1, r2 ) ->
+                                        AgentState.updateState( r1, r2, channels) )  */
+                        );
         }
 
     }
