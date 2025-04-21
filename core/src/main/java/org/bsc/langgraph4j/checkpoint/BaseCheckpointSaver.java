088785ad18b9050c1f960a2600d7fc75697831b7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-04-22 01:48:02 +0200

    feat(BaseCheckpointSaver.java): add release method
    - Introduce `THREAD_ID_DEFAULT` to provide a default thread ID.
    - Add a record type `Tag` to group checkpoints by thread ID.
    
    work on #115

--- core/src/main/java/org/bsc/langgraph4j/checkpoint/BaseCheckpointSaver.java
+++ core/src/main/java/org/bsc/langgraph4j/checkpoint/BaseCheckpointSaver.java
@@ -2,12 +2,26 @@ package org.bsc.langgraph4j.checkpoint;
 
 import org.bsc.langgraph4j.RunnableConfig;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
+
+import static java.util.Collections.unmodifiableCollection;
 
 public interface BaseCheckpointSaver {
+    String THREAD_ID_DEFAULT = "$$default";
 
-    Collection<Checkpoint> list( RunnableConfig config );
-    Optional<Checkpoint> get( RunnableConfig config );
-    RunnableConfig put( RunnableConfig config, Checkpoint checkpoint ) throws Exception;
+    record Tag(String threadId, Collection<Checkpoint> checkpoints) {
+        public Tag(String threadId, Collection<Checkpoint> checkpoints) {
+            this.threadId = threadId;
+            this.checkpoints = unmodifiableCollection(checkpoints);
+        }
+    }
+
+    Collection<Checkpoint> list(RunnableConfig config);
+
+    Optional<Checkpoint> get(RunnableConfig config);
+
+    RunnableConfig put(RunnableConfig config, Checkpoint checkpoint) throws Exception;
+
+    Tag release(RunnableConfig config) throws Exception;
+
 }
