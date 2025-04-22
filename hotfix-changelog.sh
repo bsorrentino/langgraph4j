da8938a2ab91ce545a57b1b19fe1bfd82ce17b76
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-04-22 18:07:37 +0200

    refactor(hotfix-changelog.sh): update script for changelog hotfix
    - Renamed branch name from "changenme" to "changelog"

--- hotfix-changelog.sh
+++ hotfix-changelog.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # prepare the CHANGELOG.md with the current version
-git flow hotfix start changeme
+git flow hotfix start changelog
 
 # generate CHANGELOG.md 
 git-changelog-command-line -of CHANGELOG.md
 
-git commit -m'docs: update changeme' -a
+git commit -m'docs: update changelog' -a
 
 # finish the hotfix without create tag
-git flow hotfix finish changeme -n -m"changelog hotfix merge"
+git flow hotfix finish changelog -n -m"changelog hotfix merge"
