b6ad73bb8b9c5ba134f401341fb6a2662f4e57e6
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-04-23 18:32:22 +0200

    feat: promote spring.ai agent executor as first class module

---
+++ spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/DemoConsoleController.java
@@ -0,0 +1,58 @@
+package org.bsc.langgraph4j.spring.ai.agentexecutor;
+
+import org.bsc.langgraph4j.NodeOutput;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+/**
+ * Demonstrates the use of Spring Boot CLI to execute a task using an agent executor.
+ */
+@Controller
+public class DemoConsoleController implements CommandLineRunner {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DemoConsoleController.class);
+
+    private final ChatService chatService;
+
+    public DemoConsoleController(@Qualifier("ollama") ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    /**
+     * Executes the command-line interface to demonstrate a Spring Boot application.
+     * This method logs a welcome message, constructs a graph using an agent executor,
+     * compiles it into a workflow, invokes the workflow with a specific input,
+     * and then logs the final result.
+     *
+     * @param args Command line arguments (Unused in this context)
+     * @throws Exception If any error occurs during execution
+     */
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Welcome to the Spring Boot CLI application!");
+
+        var graph = AgentExecutor.builder()
+                        .chatService( chatService )
+                        .build();
+
+        var workflow = graph.compile();
+
+        var result = workflow.stream( Map.of( "messages", new UserMessage("what is the result of 234 + 45") ));
+
+        var state = result.stream()
+                .peek( s -> System.out.println( s.node() ) )
+                .reduce((a, b) -> b)
+                .map( NodeOutput::state)
+                .orElseThrow();
+
+        log.info( "result: {}", state.lastMessage()
+                                    .map(AssistantMessage.class::cast)
+                                    .map(AssistantMessage::getText)
+                                    .orElseThrow() );
+    }
+}
