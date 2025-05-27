ca73d5be1d692bd2f8d13d713558e31464d72732
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-05-27 20:43:07 +0200

    refactor: update project site using mkdocs
    - add build script :
       - start maven site
       - copy javadoc before build
       - copy notebbok before site build
    
    merge PR #150

--- docs/release-notes.md
+++
@@ -1,3 +0,0 @@
-Please find the latest release notes [here].
-
-[here]: https://github.com/bsorrentino/langgraph4j/releases
--- site-run.sh
+++ site-run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+source .docsenv/bin/activate
+
 # Build sites and stage results
 mvn site:site site:stage -T1
 # Run site staging 
-mvn -B site:run -DworkingDirectory=target/staging
+# mvn -B site:run -DworkingDirectory=target/staging
+
+cp -r src/site/mkdocs target/mkdocs
+cp how-tos/*.ipynb target/mkdocs/how-tos
+cp -r target/staging/apidocs target/mkdocs/apidocs
+
+mkdocs build
+
+mkdocs serve 
+
